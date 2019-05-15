d1ebca2aa0cf5f3d0f749b71a1988a4b2942cbd4
kca8869 <karmijo8869@gmail.com>
2019-05-14 21:21:45 -0500

    smooth scroll and mobile nav toggle

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 var navbar=document.getElementById('header-nav');
+var navbarList=document.querySelector(".header-nav_list")
 var header=document.getElementById('header');
 var modal=document.getElementById('modal');
 var modalImg=document.getElementById('modal__img');
 var modalDis=false;
+var mobileBar=document.querySelector(".mobile-bars");
 modal.style.display="none";
+
+mobileBar.onclick=function () {
+  this.classList.toggle("bgtransparent");
+
+  if (navbar.style.top==="0px") {
+    navbar.style.top="-100%";
+  }else{
+    navbar.style.top=0;
+
+  }
+}
+window.addEventListener("resize",function () {
+  console.log(window.innerwidth)
+  if (window.innerWidth>600) {
+    navbar.style.top=0;
+  }else{
+    navbar.style.top="-100%";
+  }
+});
 
 window.addEventListener("scroll",function (e) {
     if (window.scrollY>100) {
@@ -29,6 +50,3 @@ for (var i = 0; i < cardImgs.length; i++) {
     modalDis=true;
   }
 }
-
-
-console.log(cardImgs);
